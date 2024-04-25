17837241d336423a97ce15744bf0b06bd67396e7
Leejungwoo <soulpc123@gmail.com>
2024-04-25 17:59:27 +0900

    Article 생성

--- api_pjt/articles/views.py
+++ api_pjt/articles/views.py
@@ -2,16 +2,27 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, JsonResponse
 from .models import Article
 from django.core import serializers
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import ArticleSerializer
 
+HTTP_STATUS_201_CREATED = 201
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def article_list(request):
-    articles = Article.objects.all()
-    serializer = ArticleSerializer(articles, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == "POST":
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
 
 @api_view(["GET"])
 def article_detail(request, pk):
