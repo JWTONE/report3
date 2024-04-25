f341876d02b014e2afbd5e3b81dc1d5a46bc0655
Leejungwoo <soulpc123@gmail.com>
2024-04-25 11:48:08 +0900

    first update

---
+++ api_pjt/articles/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import Article
+from django.core import serializers
+
+def article_list_html(request):
+    articles = Article.objects.all()
+    context = {"articles": articles}
+    return render(request, "articles/article_list.html", context)
+
+def json_01(request):
+    articles = Article.objects.all()
+    json_res = []
+
+    for article in articles:
+        json_res.append(
+            {
+                "title": article.title,
+                "content": article.content,
+                "created_at": article.created_at,
+                "updated_at": article.updated_at,
+            }
+        )
+
+    return JsonResponse(json_res, safe=False)
+
+def json_02(request):
+    articles = Article.objects.all()
+    res_data = serializers.serialize("json", articles)
+    return HttpResponse(res_data, content_type="application/json")
+
+def json_drf(request):
+    pass
